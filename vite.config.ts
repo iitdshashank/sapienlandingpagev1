140589e1d21240437e0e17eb33ab1fd619c4118f
Shashank Choudhary <98643273+iitdshashank@users.noreply.github.com>
2025-11-28 23:16:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
+})
+
+
   export default defineConfig({
     plugins: [react()],
     resolve: {
